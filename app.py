5fcc54a72453f143354766637953c2619415633d
chatnatee <chatnatee_khamlue@hotmail.com>
2022-04-07 19:48:30 -0400

    set up inventory as JSON

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 from flask import Flask
 
+#to see the result type "flask run" into the terminal
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #<-- called "Decorator"
 def home():
     return 'This works!'
+
+@app.route('/inventory')
+def inventory(): #<-- create a dictonary using JSON method/format (in postman use localhost5000:/inventory to access the dictionary)
+    stock = {
+        "stock":
+        [
+            {
+                "name":"healing potion",
+                "qty": 5,
+                "price": 200
+            },
+            {
+                "name":"speed potion",
+                "qty": 2,
+                "price": 500
+            },
+            {
+                "name":"polymorph potion",
+                "qty": 1,
+                "price": 5000
+            }
+        ]
+    }
+
+    return stock
