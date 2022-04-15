321575e9c9b7214e92e08a530f068e4dbd369da9
chatnatee <chatnatee_khamlue@hotmail.com>
2022-04-14 20:19:01 -0400

    request and static model

--- app.py
+++ app.py
@@ -1,34 +1,54 @@
-from flask import Flask
+from flask import Flask, request
+from staticModel import STOCK, getAllPotions, getPotionById
 
 #to see the result type "flask run" into the terminal
 
 app = Flask(__name__)
 
-@app.route('/') #<-- called "Decorator"
+@app.route('/')#<-- called "Decorator"
 def home():
     return 'You are now home'
 
 @app.route('/inventory')
 def inventory(): #<-- create a dictonary using JSON method/format (in postman use localhost5000:/inventory to access the dictionary)
     stock = {
-        "stock":
-        [
-            {
-                "name":"healing potion",
-                "qty": 5,
-                "price": 200
-            },
-            {
-                "name":"speed potion",
-                "qty": 2,
-                "price": 500
-            },
-            {
-                "name":"polymorph potion",
-                "qty": 1,
-                "price": 5000
-            }
-        ]
+        "potions": getAllPotions()
     }
-
     return stock
+
+@app.route('/potions/<id>')
+def potionByRoute(id):
+    result = getPotionById(int(id))
+    if result:
+        return result
+    else:
+        return{"error":"No potion with that ID found."}
+
+@app.route('/potions')
+def potionByParam():
+    id = request.args['id']
+    result = getPotionById(int(id))
+    if result:
+        return result
+    else:
+        return{"error":"No potion with that ID found."}
+
+@app.route('/potions', methods=['POST'])
+def potionByPost():
+    id = request.json['id']
+    result = getPotionById(int(id))
+    if result:
+        return result
+    else:
+        return{"error":"No potion with that ID found."}
+
+@app.route('/inventory/<catagory>/<id>')
+def lookupItemByRoute(catagory,id):
+    if catagory.lower() == 'potions':
+        result = getPotionById(int(id))
+        if result:
+            return result
+        else:
+            return {"error":f"{id} is not a valid id {catagory}"}
+    else:
+        return {"error":f"{id} is not a valid id {catagory}"}
